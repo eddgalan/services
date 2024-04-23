333af3b6b8224cfe8937c51c5de442dc12d207b6
Edson Galan <edd.galan@hotmail.com>
2024-04-22 19:10:33 -0600

    Curso 'Buenas prácticas' - TDD: Agrega métodos y pruebas

--- apache2/pages/cursoPHP/buenasPracticas/TestDrivenDevelopment/src/Calculator.php
+++ apache2/pages/cursoPHP/buenasPracticas/TestDrivenDevelopment/src/Calculator.php
@@ -15,4 +15,46 @@ class Calculator
     {
         return $number1 + $number2;
     }
+
+    /**
+     * Return the susbtraction of two numbers
+     * 
+     * @param int $number1
+     * @param int $number2
+     * @return int
+     */
+    public function substraction(int $number1, int $number2) : int
+    {
+        return $number1 - $number2;
+    }
+
+    /**
+     * Return the product of two numbers
+     * 
+     * @param int $number1
+     * @param int $number2
+     * @return int
+     */
+    public function multiplication(int $number1, int $number2) : int
+    {
+        return $number1 * $number2;
+    }
+
+    /**
+     * Return the quotient of a division
+     * 
+     * If the divisor is zero, returns 0
+     * 
+     * @param int $number1
+     * @param int $number2
+     * @return int
+     */
+    public function division(int $dividend, int $divisor) : int
+    {
+        if ($divisor === 0) {
+            return 0;
+        }
+        return $dividend / $divisor;
+    }
+
 }
--- apache2/pages/cursoPHP/buenasPracticas/TestDrivenDevelopment/tests/CalculatorTest.php
+++ apache2/pages/cursoPHP/buenasPracticas/TestDrivenDevelopment/tests/CalculatorTest.php
@@ -5,7 +5,10 @@ use App\Calculator;
 
 class CalculatorTest extends TestCase
 {
+    const NUMBER1 = 10;
+    const NUMBER2 = 5;
 
+    /** @var Calculator */
     private $calculator;
 
     protected function setUp(): void
@@ -15,11 +18,35 @@ class CalculatorTest extends TestCase
 
     public function testSumTwoNumbers()
     {
-        $number1 = mt_rand();
-        $number2 = mt_rand();
-        $this->assertEquals($number1 + $number2, $this->calculator->add($number1, $number2));
+        $this->assertEquals(self::NUMBER1 + self::NUMBER2, $this->calculator->add(self::NUMBER1, self::NUMBER2));
     }
 
+    public function testSubstraction()
+    {
+        $this->assertEquals(self::NUMBER1 - self::NUMBER2, $this->calculator->substraction(self::NUMBER1, self::NUMBER2));
+    }
 
+    public function testMultiplyTwoNumbers()
+    {
+        $this->assertEquals(self::NUMBER1 * self::NUMBER2, $this->calculator->multiplication(self::NUMBER1, self::NUMBER2));
+    }
+
+    public function testDivision()
+    {
+        $this->assertEquals(self::NUMBER1 / self::NUMBER2, $this->calculator->division(self::NUMBER1, self::NUMBER2));
+    }
+
+    /*
+    public function testDivisionWhenDenominatorIsZero()
+    {
+        $this->expectException(DivisionByZeroError::class);
+        $this->calculator->division(10, 0);
+    }
+    */
+
+    public function testDivisionWhenDenominatorIsZero()
+    {
+        $this->assertEquals(0, $this->calculator->division(self::NUMBER1, 0));
+    }
 
 }
